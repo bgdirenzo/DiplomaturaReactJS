c6840bbfc8be972d7f144caf74e967ae6557f6ef
bdirenzo <barbara.direnzo@gba.gob.ar>
2023-03-15 12:21:12 -0300

    Migracion de la web Transportes X

--- M4U1/transportes/frontend/src/pages/nosotrosPage.js
+++ M4U1/transportes/frontend/src/pages/nosotrosPage.js
@@ -17,6 +17,18 @@ const NosotrosPage = (props) => {
                         <h6>Gerente General</h6>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </div>
+                    <div className="persona">
+                        <img src="img/nosotros/nosotros2.jpg" alt="Ana Gomez" />
+                        <h5>Ana Gomez</h5>
+                        <h6>Gerente General</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="persona">
+                        <img src="img/nosotros/nosotros3.jpg" alt="Paul Gomez" />
+                        <h5>Paul Gomez</h5>
+                        <h6>Gerente General</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
                 </div>
             </div>
         </main>
