cd83b658defd67c040eafd2c3a8949469a45c4b4
bdirenzo <barbara.direnzo@gba.gob.ar>
2023-04-03 11:56:14 -0300

    Tarea M5U3 Finalizada. Se creo y conecto la bd correctamente

--- M4/M4U1-U2/transportes/backend/app.js
+++ M4/M4U1-U2/transportes/backend/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var pool = require('./models/bd');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
@@ -24,13 +27,33 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 
+/* INICIO Seccion de consultas */
+
+//INSERT
+// var obj = {
+//   titulo: 'Mejora',
+//   subtitulo: 'Se mejora la plataforma',
+//   cuerpo: 'La nueva mejora consiste en que podrán realizar el check-in de forma online'
+// }
+
+// pool.query('INSERT INTO novedad SET ?', [obj]).then(function (resultado) {
+//   console.log(resultado)
+// });
+
+//SELECT
+// pool.query('SELECT * FROM novedad').then(function (resultado) {
+//   console.log(resultado)
+// });
+
+/*FIN Sección consultas */
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
