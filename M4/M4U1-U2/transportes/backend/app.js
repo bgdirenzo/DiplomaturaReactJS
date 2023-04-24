b63c8b517886092b07dc032d0ba5829402092730
bdirenzo <barbara.direnzo@gba.gob.ar>
2023-04-24 12:19:51 -0300

    CRUD imagen

--- M4/M4U1-U2/transportes/backend/app.js
+++ M4/M4U1-U2/transportes/backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
 
 require('dotenv').config();
 var pool = require('./models/bd');
@@ -44,6 +45,11 @@ secured = async (req, res, next) => { //funcion utilizada como middleware
     console.log(error)
   }
 }
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
