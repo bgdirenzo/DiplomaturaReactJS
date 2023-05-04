aa9529688840640d21b3dd2d0e9a5754ffe19f93
bdirenzo <barbara.direnzo@gba.gob.ar>
2023-05-04 14:12:14 -0300

    clase mail. continuar

--- M4/M4U1-U2/transportes/backend/routes/api.js
+++ M4/M4U1-U2/transportes/backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 /*GET Home Page*/
 
--- M4/M4U1-U2/transportes/frontend/src/pages/ContactoPage.js
+++ M4/M4U1-U2/transportes/frontend/src/pages/ContactoPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../styles/components/pages/contactoPage.css';
+import {useState} from "react";
 
 const ContactoPage = (props) => {
     return (
