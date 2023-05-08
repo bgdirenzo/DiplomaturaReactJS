d8b4f6adab1d46f5ceb5b0cae3c7631c69cc4e12
bdirenzo <barbara.direnzo@gba.gob.ar>
2023-05-08 11:13:24 -0300

    Envio de formulario finalizado

--- M4/M4U1-U2/transportes/backend/routes/api.js
+++ M4/M4U1-U2/transportes/backend/routes/api.js
@@ -32,4 +32,35 @@ router.get('/novedades', async function (req, res, next) {
 
 });
 
+
+/*POST Home Page*/
+
+router.post('/contacto', async (req,res)=> {
+  const mail = {
+    to: 'barbara.direnzo@gba.gob.ar',
+    subject: 'contacto web',
+    html: `${req.body.nombre} se contactó a traves de la web y quiere más información a este correo: ${req.body.email}
+          <br/> Además, hizo el siguiente comentario: ${req.body.mensaje}
+          <br/> Su teléfono es: ${req.body.telefono}`
+  }
+
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error: false,
+    message: 'Mensaje enviado'
+  });
+
+});
+
+
 module.exports = router;
